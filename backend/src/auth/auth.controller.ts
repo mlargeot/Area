a3838e3dc8c376f196e618d0ed7cd7095823a76c
Diabolo <alexis.boitel@epitech.eu>
2024-11-29 17:52:22 +0100

    feat(backend) add swagger apiresponse and apibody

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,24 +4,32 @@ import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { ApiBody, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
+  @ApiResponse({ status: 200, description: 'Registration successful' })
+  @ApiBody({ type: CreateUserDto })
   async register(@Body() createUserDto: CreateUserDto) {
     console.log('register');
     return this.authService.register(createUserDto);
   }
 
   @Post('login')
+  @ApiResponse({ status: 200, description: 'Login successful' })
+  @ApiBody({ type: LoginUserDto })
   async login(@Body() loginUserDto: LoginUserDto) {
     console.log('login');
     return this.authService.login(loginUserDto);
   }
 
   @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  @ApiResponse({ status: 200, description: 'Logout successful' })
+  @ApiBody({ type: LoginUserDto })
   async logout(@Body() user) {
     console.log('logout');
     return this.authService.logout(user);
@@ -29,12 +37,16 @@ export class AuthController {
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
+  @ApiResponse({ status: 200, description: 'Google authentication successful' })
+  @ApiBody({ type: CreateUserDto })
   async googleAuth(@Req() req) {
     console.log('googleAuth');
   }
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
+  @ApiResponse({ status: 200, description: 'Google authentication successful' })
+  @ApiBody({ type: CreateUserDto })
   async googleAuthRedirect(@Req() req) {
     console.log('googleAuthRedirect');
     return this.authService.googleLogin(req.user);
@@ -42,12 +54,22 @@ export class AuthController {
 
   @Get('discord')
   @UseGuards(AuthGuard('discord'))
+  @ApiResponse({
+    status: 200,
+    description: 'Discord authentication successful',
+  })
+  @ApiBody({ type: CreateUserDto })
   async discordAuth(@Req() req) {
     console.log('discordAuth');
   }
 
   @Get('discord/callback')
   @UseGuards(AuthGuard('discord'))
+  @ApiResponse({
+    status: 200,
+    description: 'Discord authentication successful',
+  })
+  @ApiBody({ type: CreateUserDto })
   async discordAuthRedirect(@Req() req) {
     console.log('discordAuthRedirect');
     return this.authService.discordLogin(req.user);
@@ -55,6 +77,8 @@ export class AuthController {
 
   @Get('protected')
   @UseGuards(JwtAuthGuard)
+  @ApiResponse({ status: 200, description: 'Protected resource' })
+  @ApiBody({ type: CreateUserDto })
   async protectedResource() {
     console.log('protectedResource');
     return 'Protected resource';
