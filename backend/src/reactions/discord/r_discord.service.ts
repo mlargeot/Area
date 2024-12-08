f34541fc7f88f5295797b427a3e7b44dcc5be1fc
mlargeot <morgan.largeot@epitech.eu>
2024-12-08 11:15:04 +0100

    feat(backend/reactions/discord): add send message to webhook url reaction

---
+++ backend/src/reactions/discord/r_discord.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class ReactionsDiscordService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async sendMessageToWebhook(url: string, content: string)
+  {
+    const payload = {
+        content: content,
+    }
+
+    try {
+        const response = await firstValueFrom (
+            this.httpService.post(url, payload, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }),
+        );
+        console.log("Message sent successfully: ", response.data);
+    } catch(error) {
+        console.error('Error sending webhook message:', error.response?.data || error.message);
+        throw error;
+    }
+  }
+}
