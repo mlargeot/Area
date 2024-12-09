6a5fb9937f32d97389609ca1b4ceab47481b42a4
mlargeot <morgan.largeot@epitech.eu>
2024-12-09 19:42:34 +0100

    feat(backend/applets): add creation of applet into database

--- backend/src/applets/applets.module.ts
+++ backend/src/applets/applets.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppletsService } from './applets.service';
 import { AppletsController } from './applets.controller';
+import { ActionsModule } from 'src/actions/actions.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schemas/user.schema';
+
 
 @Module({
     providers: [AppletsService],
     controllers: [AppletsController],
-    imports: [],
+    imports: [ActionsModule,
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ],
     exports: []
 })
 export class AppletsModule {}
--- backend/src/applets/applets.service.ts
+++ backend/src/applets/applets.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
-import { AppletDto, AppletBodyDto } from './dto/applets.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AppletDto, AppletBodyDto, AppletModuleDto } from './dto/applets.dto';
+import { User } from 'src/schemas/user.schema';
+import { v4 as uuidv4 } from 'uuid'
+import { ActionsService } from 'src/actions/actions.service';
 
 @Injectable()
 export class AppletsService {
-  constructor() {}
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<User>,
+    private readonly actionsService : ActionsService
+  ) {}
 
   async getApplets(): Promise<AppletDto[]> {
     return [];
@@ -14,7 +22,39 @@ export class AppletsService {
   }
 
   async createApplet(userId: string, appletDto: AppletBodyDto): Promise<AppletDto> {
-    return null;
+
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found.`);
+    }
+  
+    const newApplet: AppletDto = {
+      appletId: uuidv4(),
+      userId,
+      action: null,
+      reaction: appletDto.reaction,
+      active: appletDto.active,
+    };
+  
+    try {
+      const actionResponse = await this.actionsService.executeAction(
+        appletDto.action.name,
+        appletDto.action.params,
+      );
+  
+      const appletAction: AppletModuleDto = {
+        name: appletDto.action.name,
+        service: appletDto.action.service,
+        params: appletDto.action.params,
+      };
+      newApplet.action = appletAction;
+    } catch (error) {
+      throw new Error(`Failed to init action "${appletDto.action.name}": ${error.message}`);
+    }
+  
+    user.applets.push(newApplet);
+    await user.save();  
+    return newApplet;
   }
 
   async updateApplet(appletId: string, appletDto: AppletBodyDto): Promise<AppletDto> {
