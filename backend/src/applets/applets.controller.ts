5b4cc0dbb80c6ceb16945c17a88ff53791e4804b
mlargeot <morgan.largeot@epitech.eu>
2024-12-09 15:11:39 +0100

    feat(backend/applets): add applets CRUD + dto

--- backend/src/applets/applets.controller.ts
+++ backend/src/applets/applets.controller.ts
@@ -1,9 +1,55 @@
-import { Controller, Get, Request } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
-import { AppletsService } from './applets.service'
-
+import { Controller, Post, Param, Body, Patch, Delete, Get } from '@nestjs/common';
+import { ApiCreatedResponse, ApiForbiddenResponse, ApiNotFoundResponse, ApiOkResponse } from '@nestjs/swagger';
+import { AppletsService } from './applets.service';
+import { AppletDto, AppletBodyDto } from './dto/applets.dto';
 
 @Controller('applets')
 export class AppletsController {
-  constructor(private readonly AppletsService: AppletsService) {}
+  constructor(private readonly appletsService: AppletsService) {}
+
+  @Get('')
+  @ApiOkResponse({ description: 'Applets found.', type: [AppletDto] })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  async getApplets(): Promise<AppletDto[]> {
+    return;
+  }
+
+  @Get(':appletId')
+  @ApiOkResponse({ description: 'Applet found.', type: AppletDto })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  async getApplet(): Promise<AppletDto> {
+    return;
+  }
+
+  @Post(':userId')
+  @ApiCreatedResponse({ description: 'Applet created.', type: AppletDto })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  async createApplet(
+    @Param('userId') userId: string,
+    @Body() appletDto: AppletBodyDto,
+  ): Promise<AppletDto> {
+    return;
+  }
+
+  @Patch(':appletId')
+  @ApiOkResponse({ description: 'Applet updated.', type: AppletDto })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiNotFoundResponse({ description: 'Applet not found.' })
+  async updateApplet(
+    @Param('appletId') appletId: number,
+    @Body() appletDto: AppletBodyDto,
+  ): Promise<AppletDto> {
+    return;
+  }
+
+  @Delete(':appletId')
+  @ApiOkResponse({ description: 'Applet deleted.'})
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiNotFoundResponse({ description: 'Applet not found.' })
+  async deleteApplet(
+    @Param('appletId') appletId: number,
+  ) {
+    return;
+  }
 }
