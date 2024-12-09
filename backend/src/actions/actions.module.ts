b1689b9f4e256087abf2298d26dbb6cfbff45ef6
MaxenceLgt <maxence.largeot@epitech.eu>
2024-12-09 01:46:35 +0100

    refactor(actions.module.ts): github action service to action module

--- backend/src/actions/actions.module.ts
+++ backend/src/actions/actions.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ActionsController } from './actions.controller';
 import { ActionsService } from './actions.service';
+import { GithubActionsModule } from './github/github.actions.module';
 
 @Module({
     providers: [ActionsService],
     controllers: [ActionsController],
-    imports: [],
+    imports: [GithubActionsModule],
     exports: [ActionsService]
 })
 export class ActionsModule {}
