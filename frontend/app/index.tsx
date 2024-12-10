b2012c6744f469ab94dbf5f8489591b71428a5d6
Diabolo <alexis.boitel@epitech.eu>
2024-12-10 10:03:34 +0100

    feat(frontend) test login with google on web

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,13 +1,21 @@
+import React, { useEffect } from "react";
 import { Link } from "expo-router";
-import { Platform, View } from "react-native";
+import { Linking, Platform, View } from "react-native";
 import { Button } from "tamagui";
 
 export default function Home() {
+  const signInWithGoogle = async () => {
+    const url = `http://localhost:8080/auth/google?device=${Platform.OS}`;
+    Linking.openURL(url);
+  }
+
   return (
     <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
       <Link href={"/explore"}>
         <Button color="green">{Platform.OS === 'web' ? "web" : "mobile"}</Button>
       </Link>
+      <Button color="red" onPress={signInWithGoogle}>Google</Button>
+      <Button color="blue">Discord</Button>
     </View>
   );
 }
