1486528d9f7ed867ed9efdc642201b2e06bab105
mlargeot <morgan.largeot@epitech.eu>
2025-01-03 15:22:39 +0100

    refactor(github.action.service): add code documentation + fix params & security

--- backend/src/automation/services/github.action.service.ts
+++ backend/src/automation/services/github.action.service.ts
@@ -1,14 +1,17 @@
 import { 
     BadRequestException,
     Injectable, 
-    NotFoundException
+    UnauthorizedException
 } from '@nestjs/common';
 import {
     User, 
     UserDocument
 } from 'src/schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import {
+    Model,
+    Types
+} from 'mongoose';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { lastValueFrom } from 'rxjs';
@@ -21,10 +24,66 @@ export class GithubActionsService {
     private configService: ConfigService,
   ) {}
 
-  async initPullRequestWebhook(userId: string, params: {name: string, githubRepoUrl: string}) {
-    const { name, githubRepoUrl } = params;
-    const user = await this.userModel.findOne({ _id: userId });
+  /**
+  * Checks if an existing webhook with the same configuration already exists for a given user.
+  * This helps prevent duplicate webhook or applet creation for the same repository and event type.
+  * 
+  * @param userId - The ID of the user in the database (retrieved from the token).
+  * @param githubRepoUrl - The URL of the repository being targeted.
+  * @param trigger - The event type that triggers the webhook (e.g., "pull_request").
+  * @returns The metadata of a matching applet if found, otherwise null.
+  */
+  async isExistingWebhook(
+    userId: string,
+    githubRepoUrl: string,
+    trigger: string
+  ): Promise<Record<string, any> | null> {
+    const result = await this.userModel.aggregate([
+      { $match: { _id: new Types.ObjectId(userId) }, },
+      { $unwind: '$applets', },
+      {
+        $match: {
+          'applets.action.params.githubRepoUrl': githubRepoUrl,
+          'applets.metadata.response.events': { $in: [trigger] },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          metadata: '$applets.metadata',
+        },
+      },
+    ]);
 
+    if (result.length > 0)
+      return result[0].metadata;
+    else
+      return null;
+  }
+
+  /**
+   * Initializes a GitHub webhook for the `pull_request` event type on a specified repository.
+   * This webhook is used to trigger applets when specific actions occur on the repository.
+   *
+   * @param userId - The ID of the user in the database (retrieved from the token).
+   * @param params - An object containing:
+   *   - `name`: The name of the webhook to create.
+   *   - `githubRepoUrl`: The URL of the GitHub repository where the webhook will be created.
+   *
+   * @returns The response data from the GitHub API after creating the webhook.
+   *
+   * @throws BadRequestException - If the provided `githubRepoUrl` is invalid.
+   * @throws NotFoundException - If the user's GitHub access token is not found.
+   * @throws Error - If the GitHub API call to create the webhook fails, with the error message.
+   */
+  async initPullRequestWebhook(userId: string, params: {githubRepoUrl: string}) {
+    const { githubRepoUrl } = params;
+
+    const existingWebhook = await this.isExistingWebhook(userId, githubRepoUrl, "pull_request");
+    if (existingWebhook)
+      return existingWebhook["response"];
+
+    const user = await this.userModel.findOne({ _id: userId });
     const regex = /https:\/\/github\.com\/([^\/]+)\/([^\/]+)/;
     const match = githubRepoUrl.match(regex);
     if (!match)
@@ -32,15 +91,15 @@ export class GithubActionsService {
 
     const githubProvider = user.oauthProviders?.find((provider) => provider.provider === 'github');
     if (!githubProvider || !githubProvider.accessToken)
-      throw new NotFoundException(`GitHub access token not found for user with ID ${userId}.`);  
+      throw new UnauthorizedException(`GitHub access token not found for user with ID ${userId}.`);  
 
     const githubAccessToken = githubProvider.accessToken;
 
     const data = {
-      name: name,
+      name: "web",
       config: {
         url: this.configService.get<string>('WEBHOOK_ENDPOINT') + "webhook/github/pr",
-        content_type: 'json',
+        content_type: "json"
       },
       events: ["pull_request"],
       active: true
