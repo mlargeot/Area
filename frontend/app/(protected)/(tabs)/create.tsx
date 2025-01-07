48d12ac78fe2c35afc1a8c423744c539bb8e49bb
Raphaël <raphael.mabille@epitech.eu>
2025-01-07 15:01:56 +0000

    feat(frontend/create): add delete button and extract core logic into functions

--- frontend/app/(protected)/(tabs)/create.tsx
+++ frontend/app/(protected)/(tabs)/create.tsx
@@ -12,7 +12,7 @@ const emptyReaction = () => {
 }
 
 const isActionValid = (applet : Applet) => {
-  return applet.action.service !== "" && applet.action.name !== "" && applet.action._id !== ""
+  return applet.action.service !== "" && applet.action.name !== "" && applet.action._id !== "" && applet.reactions.length > 0
 }
 
 export default function CreateScreen() {
@@ -20,6 +20,44 @@ export default function CreateScreen() {
   const serverAddress = useRef<string>("");
   const accessToken = useRef<string>("");
   
+
+  const saveApplet = () => {
+    if (serverAddress.current === "" || !isActionValid(applet)) { return; }
+
+    const url = `${serverAddress.current}/applets`;
+    const appletData = {
+      action: {
+        service: applet.action.service.toLowerCase(),
+        name: applet.action.name,
+        params: applet.action.params.reduce((acc, obj) => {
+          return { ...acc, ...obj };
+        }, {})
+      },
+      reaction: {
+        service: applet.reactions[0].service.toLowerCase(),
+        name: applet.reactions[0].name,
+        params: applet.reactions[0].params.reduce((acc, obj) => {
+          return { ...acc, ...obj };  
+        }, {})
+      },
+      active: true
+    }
+    console.log("URL:", url, "DATA:", appletData, "TOKEN:", accessToken.current);
+
+    axios.post(url, appletData, {headers: { Authorization: `Bearer ${accessToken.current}` }}).catch((error) => {
+      console.log("Error while saving applet, the parameters given might be invalid", error);
+    });
+  };
+
+  const emptyApplet = () => {
+    setApplet({appletId: "",action: { service: "", name: "", _id: "", params: [] }, reactions: []})
+  }
+
+  const deleteReaction = (index: number) => {
+    const newReactions = applet.reactions.filter((_, i) => i !== index);
+    setApplet({ ...applet, reactions: newReactions });
+  }
+
   useEffect(() => {
     try {
       AsyncStorage.getItem("serverAdress").then((value) => {
@@ -48,43 +86,18 @@ export default function CreateScreen() {
         <ActionButton index={1}/>
         <Square height={20} width={4} backgroundColor="$color" opacity={0.5} />
         {applet.reactions.flatMap((reaction, i) => [
-          <ReactionButton key={`card-${reaction._id}`} index={2 + i} reaction={reaction} />,
+          <ReactionButton key={`card-${reaction._id}`} index={2 + i} reaction={reaction} deleteReaction={() => {deleteReaction(i)}}/>,
           <Square key={`square-${reaction._id}`} height={20} width={4} backgroundColor="$color" opacity={0.5} />,
         ])}
         <ReactionButton index={2 + applet.reactions.length} reaction={emptyReaction()}/>
         <Button
-          onPress={() => {
-            if (serverAddress.current !== "" && isActionValid(applet)) {
-              const appletData = {
-                action: {
-                  service: applet.action.service.toLowerCase(),
-                  name: applet.action.name,
-                  params: applet.action.params.reduce((acc, obj) => {
-                    return { ...acc, ...obj };
-                  }, {})
-                },
-                reaction: {
-                  service: applet.reactions[0].service.toLowerCase(),
-                  name: applet.reactions[0].name,
-                  params: applet.reactions[0].params.reduce((acc, obj) => {
-                    return { ...acc, ...obj };
-                  }, {})
-                },
-                active: true
-              }
-              const url = `${serverAddress.current}/applets`;
-              console.log("URL:", url, "DATA:", appletData, "TOKEN:", accessToken.current);
-              axios.post(url, appletData, {headers: { Authorization: `Bearer ${accessToken.current}` }})
-            }
-          }}
+          onPress={saveApplet}
           width="80%" marginTop="$4"
         >
           <Text color="white">Save Applet</Text>
         </Button>
-        <Button 
-          onPress={() => {
-            setApplet({appletId: "",action: { service: "", name: "", _id: "", params: [] }, reactions: []})
-          }}
+        <Button
+          onPress={emptyApplet}
           width="80%" marginTop="$2"
         >
           <Text color="white">Discard Applet</Text>
@@ -93,4 +106,3 @@ export default function CreateScreen() {
     </ScrollView>
   )
 }
-
--- frontend/components/reactionButton.tsx
+++ frontend/components/reactionButton.tsx
@@ -2,47 +2,55 @@ import { Button, Stack, YStack, Text, XStack } from 'tamagui';
 import { Link } from 'expo-router';
 import { useNavigationData } from '../context/navigationContext';
 import { Reaction } from "../context/appletContext";
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Delete } from '@tamagui/lucide-icons'
 
 
-export function ReactionButton({ index, reaction } : { index : number, reaction: Reaction }) {
+export function ReactionButton({ index, reaction, deleteReaction } : { index : number, reaction: Reaction, deleteReaction?: () => void }) {
   const { setNavigationData } = useNavigationData();
   const [page, setPage] = useState<string>(reaction.service === "" ? "services" : "reaction/form");
+  const width = deleteReaction ? "70%" : "80%";
 
   return (
-    <Link href={`/Create/${page}`} asChild>
-      <Button
-      onPress={() => {
-        setNavigationData({
-          currentService: reaction.service ? reaction.service : "",
-          actionType: !reaction.service ? "reaction" : "modify",
-          reactionId: reaction._id ? reaction._id : ""
-        })}}
-      borderWidth="$1"
-      borderColor="$color"
-      padding="$3"
-      borderRadius="$2"
-      borderStyle='dotted'
-      height="fit-content"
-      width="80%"
-      justifyContent='flex-start'
-      >
-        <YStack gap="$2">
-          <XStack gap="$2">
-            <Stack
-              borderWidth="$1"
-              borderColor="$color"
-              padding="$2"
-              borderRadius="$2"
-            >
-              <Button.Text>{reaction.service !== "" ? reaction.service : 'Reaction'}</Button.Text>
-            </Stack>
-          </XStack>
-          <Text
-            opacity={0.60}
-          >{index.toString()}. { reaction.name ? reaction.name : "Select the reaction"}</Text>
-        </YStack>
-      </Button>
-    </Link>
+    <XStack width="100%" justifyContent='center'>
+      <Link href={`/Create/${page}`} asChild>
+        <Button
+        onPress={() => {
+          setNavigationData({
+            currentService: reaction.service ? reaction.service : "",
+            actionType: !reaction.service ? "reaction" : "modify",
+            reactionId: reaction._id ? reaction._id : ""
+          })}}
+        borderWidth="$1"
+        borderColor="$color"
+        padding="$3"
+        borderRadius="$2"
+        borderStyle='dotted'
+        height="fit-content"
+        width={width}
+        justifyContent='flex-start'
+        >
+          <YStack gap="$2">
+            <XStack gap="$2">
+              <Stack
+                borderWidth="$1"
+                borderColor="$color"
+                padding="$2"
+                borderRadius="$2"
+              >
+                <Button.Text>{reaction.service !== "" ? reaction.service : 'Reaction'}</Button.Text>
+              </Stack>
+            </XStack>
+            <Text
+              opacity={0.60}
+            >{index.toString()}. { reaction.name ? reaction.name : "Select the reaction"}</Text>
+          </YStack>
+        </Button>
+      </Link>
+      {deleteReaction && (
+        <Button icon={Delete} onPress={deleteReaction} width="20%" marginTop="$2">
+        </Button>
+      )}
+    </XStack>
   )
 }
