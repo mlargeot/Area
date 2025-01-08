fc015f5324604817750c66f7804ccc36a742dd67
mlargeot <morgan.largeot@epitech.eu>
2025-01-08 14:03:10 +0100

    docs(webhook.service): add github webhook services doxygen

--- backend/src/webhooks/webhook.service.ts
+++ backend/src/webhooks/webhook.service.ts
@@ -17,6 +17,20 @@ export class WebhookService {
 
 // --------------------------------------------- [GITHUB PR WEBHOOK] --------------------------------------------- // 
 
+  /**
+  * Handles the assignment of a user to a Pull Request (PR).
+  * 
+  * This method is triggered when a user is assigned to a PR. It checks for any 
+  * active applets that are subscribed to the 'pr_assigned' action for the specific 
+  * GitHub user and executes the corresponding reactions for those applets.
+  * 
+  * @param githubId - The GitHub account ID of the user who has been assigned to the PR.
+  * 
+  * @returns {Promise<boolean>} A promise that resolves to `true` if the process 
+  * completes successfully, indicating that the PR assignee event has been handled 
+  * and the associated reactions have been triggered.
+  * 
+  */
   async handlePRAssignee(githubId: number): Promise<boolean> {
     const triggeredApplets = await this.userModel.aggregate([
       {
@@ -43,6 +57,27 @@ export class WebhookService {
     return true;
   }
 
+  /**
+  * Handles Pull Request (PR) state changes (e.g., opened, closed, reopened).
+  * 
+  * This method checks the current PR state action (opened, closed, or reopened) 
+  * and triggers the corresponding reactions for any active applets that are 
+  * subscribed to this action and match the repository and owner.
+  * 
+  * @param action - The PR action that was triggered. This can be one of the following:
+  *    - 'opened'
+  *    - 'closed'
+  *    - 'reopened'
+  * 
+  * @param repositoryName - The full name of the GitHub repository where the PR 
+  * action occurred. It follows the format: `owner/repository`, where `owner` is 
+  * the GitHub username or organization name, and `repository` is the repository name.
+  * 
+  * @returns {Promise<boolean>} A promise that resolves to `true` if the process 
+  * completes successfully, indicating that the PR state event has been handled and 
+  * the associated reactions have been triggered.
+  * 
+  */
   async handlePRState(action: string, repositoryName: string): Promise<boolean> {
     const actionName = `pr_${action}`
     const owner = repositoryName.split('/')[0];
@@ -74,6 +109,27 @@ export class WebhookService {
     return true;
   }
 
+  /**
+  * Handles events triggered by Pull Requests (PR) actions.
+  * 
+  * This function processes different PR events and performs the associated 
+  * actions based on the type of PR action specified in the request body.
+  * 
+  * @param body - The event body containing the PR action and related data.
+  * The body should include an `action` field that indicates the PR event type
+  * (e.g., 'assigned', 'opened', 'closed', 'reopened', etc.), and depending on
+  * the action, the function performs corresponding tasks. 
+  * 
+  * The structure of the body will vary based on the action:
+  * - `assigned`: The body should contain an `assignee` object with an `id` field.
+  * - `opened`, `closed`, `reopened`: The body should contain a `repository` object 
+  *   with a `full_name` field representing the repository name.
+  * 
+  * @returns {Promise<boolean>} A promise that resolves to `true` if the event is
+  * successfully handled or if the event does not require further processing. 
+  * If the event is not handled, it returns `true` as the default behavior.
+  * 
+  */
   async handlePREvent(body: any) {
     if (!("action" in body))
       return true;
