b26b0dcc26ec84c32c58d037f9b4d3f122e8b911
Diabolo <alexis.boitel@epitech.eu>
2025-01-18 17:24:43 +0100

    fix(frontend) dockerfile build apk

--- frontend/apkurl.sh
+++ frontend/apkurl.sh
@@ -4,12 +4,17 @@ output=$(eas build:list --platform android --status finished --non-interactive)
 
 apk_urls=$(echo "$output" | grep -o 'https://expo.dev/artifacts/eas/[^ ]*\.apk')
 
-last_apk_url=$(echo "$apk_urls" | tail -n 1)
+first_apk_url=$(echo "$apk_urls" | head -n 1)
 
-echo "Last APK URL: $last_apk_url"
+echo "Last APK URL: $first_apk_url"
 
 # create public folder if doesnt exist
 mkdir -p public
 
 #return success even if curl fails
-curl -o public/client.apk $last_apk_url || true
+curl $first_apk_url || true
+
+# move the apk to public folder
+filename=$(basename -- "$first_apk_url")
+
+mv $filename public/client.apk || true
--- frontend/hooks/loginService.ts
+++ frontend/hooks/loginService.ts
@@ -1,22 +1,37 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as Linking from 'expo-linking';
 import {Platform} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
 
 const LoginService  = async (slug : string) : Promise<any> => {
+    const router = useRouter();
     const redirectUri = Linking.createURL('/auth/callback');
     console.log("LoginService called with slug: " + slug);
     console.log("LoginService redirectUri: " + redirectUri);
     const serverUrl = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8080';
-
     //redirect directly to backend auth endpoint
 
     if (Platform.OS === 'web') {
         window.location.href = serverUrl + "/auth/login/" + slug + "?redirect_uri=" + redirectUri;
     } else {
-        const result : any = await WebBrowser.openAuthSessionAsync(
-                serverUrl + "/auth/login/" + slug + "?redirect_uri=" + redirectUri,
+        const androidRedirectUri = Linking.createURL('/auth/callback');
+        const result: any = await WebBrowser.openAuthSessionAsync(
+        serverUrl + "/auth/login/" + slug + "?redirect_uri=" + androidRedirectUri,
         );
-        console.log("LoginService result: " + result);
+        if (result.type === 'success' && result.url) {
+            console.log("Auth session success:", result.url);
+            const urlParams = new URLSearchParams(result.url.split('?')[1]);
+            const token = urlParams.get('token');
+            if (token) {
+                console.log("Token received:", token);
+                await AsyncStorage.setItem('access_token', token);
+                router.push('/explore');
+            } else {
+                console.error("Token not found in redirect URL.");
+            }
+          }
+
     }
 }
 
