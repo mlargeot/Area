fc2e158eec047a603b4ff893bb12c1f587461d7c
MrMarmotte <114657171+MrMarmotte@users.noreply.github.com>
2025-01-18 19:08:22 +0100

    feat(feature/front_upgrade): fix scroll

--- frontend/app/(protected)/(tabs)/library.tsx
+++ frontend/app/(protected)/(tabs)/library.tsx
@@ -1,4 +1,4 @@
-import { H2, XStack, YStack, Text,Card, Paragraph, Button, Spinner  } from 'tamagui'
+import { H2, XStack, YStack, Text,Card, Paragraph, Button, Spinner, ScrollView  } from 'tamagui'
 import React, { useEffect } from 'react'
 import { useAppletList } from '../../../context/appletListContext'
 import { Link } from 'expo-router';
@@ -17,48 +17,50 @@ export default function LibraryScreen() {
   }, []);
 
   return (
-    <YStack f={1} ai="center" px="$4" pt="$5" bg="$background">
-      <H2 mb="$4">My Applets</H2>
+    <ScrollView bg="$background">
+      <YStack f={1} ai="center" px="$4" pt="$5" bg="$background">
+        <H2 mb="$4">My Applets</H2>
 
-      {appletList.length === 0 ? (
-        <Card elevate bordered p="$4" w="100%" maxWidth={600}>
-          <Paragraph ta="center">You haven't created any applets yet.</Paragraph>
-          <Button mt="$4" als="center" onPress={() => router.push({ pathname: '/create'})}>Create Your First Applet</Button>
-        </Card>
-      ) : (
-        <YStack gap="$4" w="100%" maxWidth={600}>
-          {appletList.map((applet, i) => (
-            <Link key={i} href="/create" asChild>
-              <Card
-                elevate
-                bordered
-                animation="bouncy"
-                scale={0.97}
-                hoverStyle={{ scale: 1 }}
-                pressStyle={{ scale: 0.95 }}
-                onPress={() => setApplet(applet)}
-              >
-                <Card.Header padded>
-                  <H2>{applet.name}</H2>
-                </Card.Header>
-                <Card.Footer padded>
-                  <XStack jc="space-between" ai="center" w="100%">
-                    <XStack ai="center" gap="$2">
-                      <Activity size={18} color="$blue10" />
-                      <Text color="$blue10">{applet.action.service}</Text>
+        {appletList.length === 0 ? (
+          <Card elevate bordered p="$4" w="100%" maxWidth={600}>
+            <Paragraph ta="center">You haven't created any applets yet.</Paragraph>
+            <Button mt="$4" als="center" onPress={() => router.push({ pathname: '/create'})}>Create Your First Applet</Button>
+          </Card>
+        ) : (
+          <YStack gap="$4" w="100%" maxWidth={600}>
+            {appletList.map((applet, i) => (
+              <Link key={i} href="/create" asChild>
+                <Card
+                  elevate
+                  bordered
+                  animation="bouncy"
+                  scale={0.97}
+                  hoverStyle={{ scale: 1 }}
+                  pressStyle={{ scale: 0.95 }}
+                  onPress={() => setApplet(applet)}
+                >
+                  <Card.Header padded>
+                    <H2>{applet.name}</H2>
+                  </Card.Header>
+                  <Card.Footer padded>
+                    <XStack jc="space-between" ai="center" w="100%">
+                      <XStack ai="center" gap="$2">
+                        <Activity size={18} color="$blue10" />
+                        <Text color="$blue10">{applet.action.service}</Text>
+                      </XStack>
+                      <ChevronRight size={18} color="$gray10" />
+                      <XStack ai="center" gap="$2">
+                        <Zap size={18} color="$orange10" />
+                        <Text color="$orange10">{applet.reactions[0].service}</Text>
+                      </XStack>
                     </XStack>
-                    <ChevronRight size={18} color="$gray10" />
-                    <XStack ai="center" gap="$2">
-                      <Zap size={18} color="$orange10" />
-                      <Text color="$orange10">{applet.reactions[0].service}</Text>
-                    </XStack>
-                  </XStack>
-                </Card.Footer>
-              </Card>
-            </Link>
-          ))}
-        </YStack>
-      )}
-    </YStack>
+                  </Card.Footer>
+                </Card>
+              </Link>
+            ))}
+          </YStack>
+        )}
+      </YStack>
+    </ScrollView>
   )
 }
